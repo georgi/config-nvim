c42caea0c3e104757209296c5451abda57c854fa
Matthias Georgi <matti.georgi@gmail.com>
2023-02-04 11:58:32 +0100

    enable auto suggestions from copilot

--- lua/packages.lua
+++ lua/packages.lua
@@ -236,7 +236,18 @@ require("lazy").setup({
   end},
   { "zbirenbaum/copilot.lua", config = function()
       require("copilot").setup({
-        suggestion = { enabled = false },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<C-n>",
+            prev = "<C-p>",
+            dismiss = "<C-]>",
+          }
+        },
         panel = { enabled = false },
       })
   end},
